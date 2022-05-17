7e04a0020cedc2e06865fe3072700a7c34cb4c4f
Hunseong <phunseong@gmail.com>
2022-05-18 03:36:06 +0900

    initial commit

---
+++ src/main/java/com/hunseong/lolcruit/LolcruitApplication.java
@@ -0,0 +1,13 @@
+package com.hunseong.lolcruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolcruitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolcruitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hunseong/lolcruit/LolcruitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunseong.lolcruit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolcruitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
