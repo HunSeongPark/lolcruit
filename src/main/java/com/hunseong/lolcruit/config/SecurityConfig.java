37fbf9712130bf867f6e478d7fdc357c884c81ee
Hunseong <phunseong@gmail.com>
2022-05-23 17:11:38 +0900

    normal login - SecurityConfig

---
+++ src/main/java/com/hunseong/lolcruit/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.hunseong.lolcruit.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by Hunseong on 2022/05/23
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.authorizeRequests()
+                .antMatchers("/api/posts/**").authenticated()
+                .antMatchers("/posts/add/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/loginForm")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/");
+    }
+}
