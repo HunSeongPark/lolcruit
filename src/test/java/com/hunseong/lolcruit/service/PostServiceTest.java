97a272c5ce58d9f1776c052b938dd28bccce5c7d
Hunseong <phunseong@gmail.com>
2022-05-26 18:43:42 +0900

    UserServiceTest - join

--- src/test/java/com/hunseong/lolcruit/service/PostServiceTest.java
+++ src/test/java/com/hunseong/lolcruit/service/PostServiceTest.java
@@ -1,4 +1,4 @@
-package com.hunseong.lolcruit.service.post;
+package com.hunseong.lolcruit.service;
 
 import com.hunseong.lolcruit.domain.post.Position;
 import com.hunseong.lolcruit.domain.post.Post;
@@ -14,6 +14,7 @@ import com.hunseong.lolcruit.web.dto.post.PostRequestDto;
 import com.hunseong.lolcruit.web.dto.user.JoinRequestDto;
 import com.hunseong.lolcruit.web.dto.user.SessionUser;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
---
+++ src/test/java/com/hunseong/lolcruit/service/UserServiceTest.java
@@ -0,0 +1,51 @@
+package com.hunseong.lolcruit.service;
+
+import com.hunseong.lolcruit.web.dto.user.JoinRequestDto;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Hunseong on 2022/05/26
+ */
+@Slf4j
+@Transactional
+@SpringBootTest
+class UserServiceTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Autowired
+    private UserService userService;
+
+    @DisplayName("회원가입에 성공한다")
+    @Test
+    void join() {
+
+        // given
+        JoinRequestDto joinRequestDto = new JoinRequestDto("hunseong", "1234", "hunseong", "gnstjd@naver.com");
+
+        // when
+        userService.join(joinRequestDto);
+
+        // then
+        boolean hasNickname = userService.hasNickname(joinRequestDto.getNickname());
+        boolean hasEmail = userService.hasEmail(joinRequestDto.getEmail());
+        boolean hasUsername = userService.hasUsername(joinRequestDto.getUsername());
+
+        assertThat(hasNickname).isTrue();
+        assertThat(hasEmail).isTrue();
+        assertThat(hasUsername).isTrue();
+    }
+}
