ea55f70cd115d8eb58aa09eaafcdb89fe1908236
Hunseong <phunseong@gmail.com>
2022-05-26 18:47:10 +0900

    [refactor] UserServiceTest - join (add UserRepository)

---
+++ src/test/java/com/hunseong/lolcruit/service/CommentServiceTest.java
@@ -0,0 +1,10 @@
+package com.hunseong.lolcruit.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Hunseong on 2022/05/26
+ */
+class CommentServiceTest {
+
+}
--- src/test/java/com/hunseong/lolcruit/service/UserServiceTest.java
+++ src/test/java/com/hunseong/lolcruit/service/UserServiceTest.java
@@ -1,5 +1,7 @@
 package com.hunseong.lolcruit.service;
 
+import com.hunseong.lolcruit.domain.user.User;
+import com.hunseong.lolcruit.domain.user.UserRepository;
 import com.hunseong.lolcruit.web.dto.user.JoinRequestDto;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
@@ -12,6 +14,8 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,11 +27,11 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class UserServiceTest {
 
-    @PersistenceContext
-    EntityManager em;
-
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @DisplayName("회원가입에 성공한다")
     @Test
@@ -43,9 +47,13 @@ class UserServiceTest {
         boolean hasNickname = userService.hasNickname(joinRequestDto.getNickname());
         boolean hasEmail = userService.hasEmail(joinRequestDto.getEmail());
         boolean hasUsername = userService.hasUsername(joinRequestDto.getUsername());
+        User user = userRepository.findByUsername(joinRequestDto.getUsername())
+                        .get();
 
         assertThat(hasNickname).isTrue();
         assertThat(hasEmail).isTrue();
         assertThat(hasUsername).isTrue();
+        assertThat(user.getUsername()).isEqualTo(joinRequestDto.getUsername());
+        assertThat(user.getNickname()).isEqualTo(joinRequestDto.getNickname());
     }
 }
