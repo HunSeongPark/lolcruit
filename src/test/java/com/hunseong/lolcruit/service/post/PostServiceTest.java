d10c595c544a9d93341b8904571ce7cf3950cd92
Hunseong <phunseong@gmail.com>
2022-05-26 18:30:03 +0900

    PostServiceTest - delete fail user not found

--- src/test/java/com/hunseong/lolcruit/service/post/PostServiceTest.java
+++ src/test/java/com/hunseong/lolcruit/service/post/PostServiceTest.java
@@ -234,22 +234,24 @@ class PostServiceTest {
         assertThat(result.getPosition()).isEqualTo(postEditDto.getPosition());
     }
 
-//    @Test
-//    void delete() {
-//
-//        // given
-//        JoinRequestDto joinRequestDto = new JoinRequestDto("hunseong", "1234", "hunseong", "gnstjd@naver.com");
-//        userService.join(joinRequestDto);
-//
-//        PostRequestDto postRequestDto = new PostRequestDto("title", "content", "hunseong", Position.MID);
-//        SessionUser sessionUser = new SessionUser("hunseong", "1234", "hunseong", "gnstjd@naver.com", Role.USER);
-//
-//        Long postId = postService.add(postRequestDto, sessionUser);
-//
-//        // when
-//        Long deletedPostId = postService.delete(postId);
-//
-//        // then
-//        postService.findByIdForRead(deletedPostId);
-//    }
+    @Test
+    void delete_success() {
+
+        // given
+        JoinRequestDto joinRequestDto = new JoinRequestDto("hunseong", "1234", "hunseong", "gnstjd@naver.com");
+        userService.join(joinRequestDto);
+
+        PostRequestDto postRequestDto = new PostRequestDto("title", "content", "hunseong", Position.MID);
+        SessionUser sessionUser = new SessionUser("hunseong", "1234", "hunseong", "gnstjd@naver.com", Role.USER);
+
+        Long postId = postService.add(postRequestDto, sessionUser);
+
+        // when
+        Long deletedPostId = postService.delete(postId, sessionUser);
+
+        // then
+        CustomException customException = assertThrows(CustomException.class,
+                () -> postService.findByIdForRead(deletedPostId));
+        log.info(customException.getMessage());
+    }
 }
