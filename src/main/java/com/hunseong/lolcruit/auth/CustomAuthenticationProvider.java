72b72475e0ef1ae184acb4db22a05bb034e5b4d0
Hunseong <phunseong@gmail.com>
2022-05-28 15:36:52 +0900

    oauth2.0 - application-oauth.yml

--- src/main/java/com/hunseong/lolcruit/auth/CustomAuthenticationProvider.java
+++ src/main/java/com/hunseong/lolcruit/auth/CustomAuthenticationProvider.java
@@ -1,7 +1,5 @@
 package com.hunseong.lolcruit.auth;
 
-import com.hunseong.lolcruit.domain.user.User;
-import com.hunseong.lolcruit.domain.user.UserRepository;
 import com.hunseong.lolcruit.web.dto.user.SessionUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -9,8 +7,6 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
