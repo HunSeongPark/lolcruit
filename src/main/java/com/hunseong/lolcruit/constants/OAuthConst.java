22bf805009d672fd5cfb62d0c34331eff5725e13
Hunseong <phunseong@gmail.com>
2022-08-26 16:27:07 +0900

    refactor: gitignore

---
+++ src/main/java/com/hunseong/lolcruit/constants/OAuthConst.java
@@ -0,0 +1,8 @@
+package com.hunseong.lolcruit.constants;
+
+/**
+ * Created by Hunseong on 2022/05/28
+ */
+public class OAuthConst {
+    public static final String PASSWORD_SECRET = "lolcruit_secret_1032";
+}
